4819e38e3a9a66b8a02576d893d3640381e008e2
Haniyeh Sadat Miremadi <114092435+Haniyem@users.noreply.github.com>
2025-08-02 23:36:34 +0330

    SmsMessage.java with SOLID

--- step2-with-solid/src/edu/sharif/selab/models/SmsMessage.java
+++ step2-with-solid/src/edu/sharif/selab/models/SmsMessage.java
@@ -1,22 +1,34 @@
 package edu.sharif.selab.models;
 
-public class SmsMessage extends Message{
+public class SmsMessage implements Message {
     private String sourcePhoneNumber;
     private String targetPhoneNumber;
-
-    public String getSourcePhoneNumber() {
-        return sourcePhoneNumber;
-    }
+    private String content;
 
     public void setSourcePhoneNumber(String sourcePhoneNumber) {
         this.sourcePhoneNumber = sourcePhoneNumber;
     }
 
-    public String getTargetPhoneNumber() {
+    public void setTargetPhoneNumber(String targetPhoneNumber) {
+        this.targetPhoneNumber = targetPhoneNumber;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String getSource() {
+        return sourcePhoneNumber;
+    }
+
+    @Override
+    public String getTarget() {
         return targetPhoneNumber;
     }
 
-    public void setTargetPhoneNumber(String targetPhoneNumber) {
-        this.targetPhoneNumber = targetPhoneNumber;
+    @Override
+    public String getContent() {
+        return content;
     }
 }
