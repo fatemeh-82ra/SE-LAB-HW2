da1a293ff8aaaeeecac08cea372bffa475663399
Haniyeh Sadat Miremadi <114092435+Haniyem@users.noreply.github.com>
2025-08-02 23:35:05 +0330

    EmailMessage.java with SOLID

--- step2-with-solid/src/edu/sharif/selab/models/EmailMessage.java
+++ step2-with-solid/src/edu/sharif/selab/models/EmailMessage.java
@@ -1,22 +1,34 @@
 package edu.sharif.selab.models;
 
-public class EmailMessage extends Message{
+public class EmailMessage implements Message {
     private String sourceEmailAddress;
     private String targetEmailAddress;
-
-    public String getSourceEmailAddress() {
-        return sourceEmailAddress;
-    }
+    private String content;
 
     public void setSourceEmailAddress(String sourceEmailAddress) {
         this.sourceEmailAddress = sourceEmailAddress;
     }
 
-    public String getTargetEmailAddress() {
+    public void setTargetEmailAddress(String targetEmailAddress) {
+        this.targetEmailAddress = targetEmailAddress;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String getSource() {
+        return sourceEmailAddress;
+    }
+
+    @Override
+    public String getTarget() {
         return targetEmailAddress;
     }
 
-    public void setTargetEmailAddress(String targetEmailAddress) {
-        this.targetEmailAddress = targetEmailAddress;
+    @Override
+    public String getContent() {
+        return content;
     }
 }
