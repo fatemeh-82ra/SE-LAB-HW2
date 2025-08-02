22d33db20b70441f0cbe5aff01d0f0c6d400f3cf
Haniyeh Sadat Miremadi <114092435+Haniyem@users.noreply.github.com>
2025-08-02 23:40:02 +0330

    SmsMessageService.java with SOLID

--- step2-with-solid/src/edu/sharif/selab/services/SmsMessageService.java
+++ step2-with-solid/src/edu/sharif/selab/services/SmsMessageService.java
@@ -1,42 +1,34 @@
-package edu.sharif.selab.services;
+package edu.sharif.selab.models;
 
-import edu.sharif.selab.models.EmailMessage;
-import edu.sharif.selab.models.SmsMessage;
+public class SmsMessage implements Message {
+    private String sourcePhoneNumber;
+    private String targetPhoneNumber;
+    private String content;
 
-public class SmsMessageService implements MessageService{
-    @Override
-    public void sendSmsMessage(SmsMessage smsMessage) {
-        if(validatePhoneNumber(smsMessage.getSourcePhoneNumber()) && validatePhoneNumber(smsMessage.getTargetPhoneNumber())){
-            System.out.println("Sending a SMS from " + smsMessage.getSourcePhoneNumber() + " to " + smsMessage.getTargetPhoneNumber() + " with content : " + smsMessage.getContent());
-        }else{
-            throw new IllegalArgumentException("Phone Number is Not Correct!");
-        }
+    public void setSourcePhoneNumber(String sourcePhoneNumber) {
+        this.sourcePhoneNumber = sourcePhoneNumber;
+    }
+
+    public void setTargetPhoneNumber(String targetPhoneNumber) {
+        this.targetPhoneNumber = targetPhoneNumber;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
     @Override
-    public void sendEmailMessage(EmailMessage emailMessage) {
-        //Empty Body!
+    public String getSource() {
+        return sourcePhoneNumber;
     }
 
     @Override
-    public void sendTelegramMessage(TelegramMessage telegramMessage) {
-        // Empty Body!
+    public String getTarget() {
+        return targetPhoneNumber;
     }
 
-    private boolean validatePhoneNumber(String phoneNumber) {
-        // Check if the phone number is exactly 11 characters long
-        if (phoneNumber.length() != 11) {
-            return false; // Phone number length is not valid
-        }
-
-        // Check if the phone number contains only numeric digits
-        for (char digit : phoneNumber.toCharArray()) {
-            if (!Character.isDigit(digit)) {
-                return false; // Phone number contains non-numeric characters
-            }
-        }
-
-        // If all checks pass, return true (valid phone number)
-        return true;
+    @Override
+    public String getContent() {
+        return content;
     }
 }
